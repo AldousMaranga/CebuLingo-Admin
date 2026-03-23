8ef465475d45bde5d2d8fac5a09f17e2b800d7bb
Aldous Maranga <your-email@example.com>
2026-03-23 13:00:12 +0800

    Refixed bug

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from './Components/Home';
-import Users from './Components/Users';
-import Lessons from './Components/Lessons';
-import Settings from './Components/Settings';
+// import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+// import Home from './Components/Home';
+// import Users from './Components/Users';
+// import Lessons from './Components/Lessons';
+// import Settings from './Components/Settings';
 import Navigation from './Components/Navigation';
 
 function App() {
